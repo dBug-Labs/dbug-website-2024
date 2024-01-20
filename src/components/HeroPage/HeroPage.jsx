4c98c0594cba761b392052da6fac48b2ee15e7db
Augnik <shresnik2004@gmail.com>
2024-01-21 00:56:14 +0530

    mobile view updt

--- src/components/HeroPage/HeroPage.jsx
+++ src/components/HeroPage/HeroPage.jsx
@@ -26,7 +26,7 @@ export default function HeroPage() {
     <>
       <div className="flex items-center justify-center h-screen">
         <div className="flex-1 text-center pb-18">
-          <h1 className='text-center pb-16 font-bold text-4xl sm:text-5xl ml-10' style={{ color: 'rgb(152, 152, 152)' }}>
+          <h1 className={`${isMobile ?  "text-center  font-bold text-4xl md:text-5xl ml-10 pb-8 pr-10": 'text-center pb-16 font-bold text-4xl md:text-5xl ml-10'}`} style={{ color: 'rgb(152, 152, 152)' }}>
             the 24/7 <span className="text-5xl font-bold bg-clip-text text-transparent" style={{ backgroundImage: 'linear-gradient(to top, rgb(22, 22, 22), rgb(152, 152, 152))' }}>debugging</span> community
           </h1>
           <div className={`w-auto ${isMobile ? 'pl-4 pr-2 ml-4 mr-4' : 'pl-8 pr-6 ml-48 mr-48'}`}>
@@ -40,7 +40,7 @@ export default function HeroPage() {
             </button>
             <img className='mx-auto pt-1' src='npx.svg'/>
             {isMobile && (
-              <img className='mx-auto pt-1 ' src={heroSecMob} alt='Hero Image' />
+              <img className='pt-14' src={heroSecMob} alt='Hero Image' />
             )}
           </div>
         </div>
